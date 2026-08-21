c9cebac45e13bf2b42c997474465926eb260e8e5
unknown <lindinha2101l@gmail.com>
2026-08-21 19:36:04 -0300

    Adicionar Estrutura e Questão 1

---
+++ questao_1.c
@@ -0,0 +1,12 @@
+/*
+Questão 01. Escreva um programa completo em C que declare uma variável do tipo inteiro, atribua um
+valor a ela (como o seu ano de nascimento ou o ano letivo corrente) e a imprima na tela junto com uma
+mensagem de texto explicativa utilizando a função printf() com o especificador de formato
+correspondente.
+*/
+#include <stdio.h>
+
+int main()
+{
+
+}
